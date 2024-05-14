0833722a6bce4a795989845ac2081764dc155c9d
Elif Dede Yildirim <elifdy@auburn.edu>
2024-05-14 03:35:18 -0400

    version 0.0.6

--- omopsurvey/__init__.py
+++ omopsurvey/__init__.py
@@ -1,4 +1,4 @@
-from omopsurvey.response_set import mapitems, createdummies, scale
+from omopsurvey.response_set import map_items, create_dummies, scale, map_answers
 from omopsurvey.codebooks import create_codebook, generate_codebook, print_codebook, codebook
 from omopsurvey.pivot_data import pivot, pivot_text
 from omopsurvey.recode_missing import recode
